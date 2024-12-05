b1b5d295ad068a7230652c970c922243dc518c5a
Kevin Ferlauto <kevinferl1998@gmail.com>
2024-12-05 08:35:38 +0100

    Fix rabbitmq-init.sh

--- rabbitmq-init.sh
+++ rabbitmq-init.sh
@@ -2,4 +2,4 @@ rabbitmqctl add_exchange exchange_event direct
 
 rabbitmqctl add_queue queue_event
 
-rabbitmqctl add_binding my_exchange my_queue --routing-key routing_key_event
+rabbitmqctl add_binding exchange_event queue_event --routing-key routing_key_event
